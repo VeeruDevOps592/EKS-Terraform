db4331ebd1049d2f3d0de631e8bbb45f3aafe646
VeeruDevOps592 <veerulucky592@gmail.com>
2025-01-19 22:04:18 +0530

    Update gather.tf

--- module/gather.tf
+++ module/gather.tf
@@ -1,21 +1,50 @@
-data "tls_certificate" "eks-certificate" {
-  url = aws_eks_cluster.eks[0].identity[0].oidc[0].issuer
-}
+# Azure AKS Cluster with AAD Integration
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  kubernetes_version  = var.cluster_version
 
-data "aws_iam_policy_document" "eks_oidc_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect  = "Allow"
+  identity {
+    type = "SystemAssigned"
+  }
 
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.eks-oidc.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:default:aws-test"]
-    }
-
-    principals {
-      identifiers = [aws_iam_openid_connect_provider.eks-oidc.arn]
-      type        = "Federated"
+  role_based_access_control {
+    enabled = true
+    azure_active_directory {
+      managed = true
+      admin_group_object_ids = var.aad_admin_group_object_ids # Admin groups in AAD
     }
   }
+
+  tags = {
+    Name = var.cluster_name
+    Env  = var.env
+  }
+}
+# Define a Custom Role (Equivalent to IAM Policy)
+resource "azurerm_role_definition" "aks_custom_role" {
+  name        = "${var.cluster_name}-custom-role"
+  scope       = data.azurerm_subscription.primary.id
+  description = "Custom role for AKS Service Account access"
+  
+  permissions {
+    actions = [
+      "Microsoft.ContainerService/managedClusters/*", # Full access to AKS clusters
+      "Microsoft.ContainerRegistry/registries/*",     # Access to container registry
+      "Microsoft.Network/virtualNetworks/*"          # Network-related permissions
+    ]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    data.azurerm_subscription.primary.id
+  ]
+}
+
+# Assign the Custom Role to AKS Managed Identity
+resource "azurerm_role_assignment" "aks_role_assignment" {
+  principal_id       = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  role_definition_id = azurerm_role_definition.aks_custom_role.id
+  scope              = data.azurerm_subscription.primary.id
 }
